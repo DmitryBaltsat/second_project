274a8d677e89440b44708697978299f3685fddf3
Dmitry Baltsat <d-baltsat@mail.ru>
2022-06-26 22:51:10 +0300

    deleted python files

--- README.md
+++
@@ -1,2 +0,0 @@
-This is done to see how info is pushed
-Now I'm trying to change through GitKraken
--- first_run.py
+++
@@ -1,4 +0,0 @@
-family = {1: 'Dima',
-        2: 'Anna',
-        3: 'Alexandra'}
-print(family[1])
